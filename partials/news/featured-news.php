5b68692f0ea641e1ea5459a831a5bc007588082b
Damilare Shobowale <oluwadamilareshobowale@gmail.com>
2021-06-09 16:48:26 +0100

    change readme.md and fix container for featured news

--- partials/news/featured-news.php
+++ partials/news/featured-news.php
@@ -6,36 +6,38 @@ $sticky_posts = News::get_instance()->get_sticky_posts();
 if(!empty($sticky_posts)) {
 
 ?>
-<section class="flex flex-col lg:flex-row frame border-t">
-    <div class="w-full lg:w-3/5 h-97">
-        <img
-            src="<?= $sticky_posts['image'] ?>"
-            alt=""
-            class="w-full h-full object-cover object-top"
-        >
-    </div>
-    <div class="w-full lg:w-2/5 pl-7 lg:pl-12 h-97">
-        <?php
+<section class="border-t">
+    <div class="custom-container flex flex-col lg:flex-row frame">
+        <div class="w-full lg:w-3/5 h-97">
+            <img
+                    src="<?= $sticky_posts['image'] ?>"
+                    alt=""
+                    class="w-full h-full object-cover object-top"
+            >
+        </div>
+        <div class="w-full lg:w-2/5 pl-7 lg:pl-12 h-97">
+            <?php
             if(isset($sticky_posts['category']['title'])) {
-        ?>
-        <p class="pt-9">
+                ?>
+                <p class="pt-9">
             <span class="inline-block bg-black text-white px-3 py-2 text-xs"
             ><?= $sticky_posts['category']['title'] ?></span
             >
-        </p>
-        <?php } ?>
-        <h2 class="pt-6 text-5xl font-black max-w-sm">
-            <?= $sticky_posts['title'] ?>
-        </h2>
-        <p class="text-lg mt-7.5 max-w-sm">
-            <?= $sticky_posts['summary'] ?>
-        </p>
-        <a
-            href="<?= $sticky_posts['link'] ?>"
-            class="text-base font-bold text-gray-4 mt-7.5 py-2 inline-block underline hover:text-red"
-            title="<?= $sticky_posts['title'] ?>"
-        ><?= __('Read more', 'film-africa-wp') ?></a
-        >
+                </p>
+            <?php } ?>
+            <h2 class="pt-6 text-5xl font-black max-w-sm">
+                <?= $sticky_posts['title'] ?>
+            </h2>
+            <p class="text-lg mt-7.5 max-w-sm">
+                <?= $sticky_posts['summary'] ?>
+            </p>
+            <a
+                    href="<?= $sticky_posts['link'] ?>"
+                    class="text-base font-bold text-gray-4 mt-7.5 py-2 inline-block underline hover:text-red"
+                    title="<?= $sticky_posts['title'] ?>"
+            ><?= __('Read more', 'film-africa-wp') ?></a
+            >
+        </div>
     </div>
 </section>
 <?php
